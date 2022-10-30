bb723b5adf0cdc6551ff1ecfde27643553fa04b3
ugurilgin <43095839+ugurilgin@users.noreply.github.com>
2022-10-30 20:28:53 +0300

    Employee Repository is Done

--- HotelManagement/HotelManagement.Data/Concrete/EmployeeRepository.cs
+++ HotelManagement/HotelManagement.Data/Concrete/EmployeeRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataAccess.Abstract;
+using HotelManagement.Data;
+using HotelManagement.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,52 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete
 {
-    internal class EmployeeRepository
+    internal class EmployeeRepository : IEmployeeRepository
     {
+        public Employee createEmployee(Employee employee)
+        {
+            using (var applicationDbContext = new ApplicationDbContext())
+            {
+                applicationDbContext.Employees.Add(employee);
+                applicationDbContext.SaveChanges();
+                return employee;
+            }
+        }
+
+        public void deleteEmployee(int id)
+        {
+            using (var applicationDbContext = new ApplicationDbContext())
+            {
+               var deletedEmployee=getEmployee(id);
+                applicationDbContext.Employees.Remove(deletedEmployee);
+                applicationDbContext.SaveChanges();
+            }
+        }
+
+        public List<Employee> getAllEmployees()
+        {
+            using (var applicationDbContext = new ApplicationDbContext())
+            {
+                return applicationDbContext.Employees.ToList();
+            }
+        }
+
+        public Employee getEmployee(int id)
+        {
+            using (var applicationDbContext = new ApplicationDbContext())
+            {
+                return applicationDbContext.Employees.Find(id);
+            }
+        }
+
+        public Employee updateEmployee(Employee employee)
+        {
+            using (var applicationDbContext = new ApplicationDbContext())
+            {
+                 applicationDbContext.Employees.Update(employee);
+                applicationDbContext.SaveChanges();
+                return employee;
+            }
+        }
     }
 }
